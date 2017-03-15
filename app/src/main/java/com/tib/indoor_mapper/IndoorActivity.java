2ccb7d69e3127677bac1f98524ef62445d416667
Imran Bohoran <imran.bohoran@digital.cabinet-office.gov.uk>
2017-03-15 01:07:44 +0000

    Initial project commit
     * Sets up the project template
     * Includes Indoor atlas dependencies
     * Includes firebase dependencies

---
+++ app/src/main/java/com/tib/indoor_mapper/IndoorActivity.java
@@ -0,0 +1,13 @@
+package com.tib.indoor_mapper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IndoorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_indoor);
+    }
+}
