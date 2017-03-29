e234f37a8ab6345238321683af9225bf25896f47
Imran Bohoran <imranbohoran@gmail.com>
2017-03-29 12:23:50 +0100

    Capturing longitude and latitude
     * Capturing the coordinates and showing it next to the alert button.

--- app/src/androidTest/java/com/tib/indoor_mapper/GeoFenceActivityTest.java
+++ app/src/androidTest/java/com/tib/indoor_mapper/GeoFenceActivityTest.java
@@ -1,6 +1,7 @@
 package com.tib.indoor_mapper;
 
 
+import android.support.test.espresso.action.ViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -9,10 +10,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class GeoFenceActivityTest {
@@ -31,5 +34,15 @@ public class GeoFenceActivityTest {
     @Test
     public void alertButtonClickable() {
         onView(withId(R.id.button_alert)).check(matches(isClickable()));
+    }
+
+    @Test
+    public void alertButtonClickShouldCaptureCoordinates() {
+        onView(withId(R.id.text_lng)).perform(ViewActions.typeText("1.34"));
+        onView(withId(R.id.text_lat)).perform(ViewActions.typeText("0.34"));
+        onView(withId(R.id.button_alert)).perform(click());
+
+        onView(withId(R.id.txt_info_current_location)).check(matches(withText("Current set location - Lng: 1.34, Lat: 0.34")));
+
     }
 }
--- app/src/main/java/com/tib/indoor_mapper/GeoFenceActivity.java
+++ app/src/main/java/com/tib/indoor_mapper/GeoFenceActivity.java
@@ -3,11 +3,26 @@ package com.tib.indoor_mapper;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class GeoFenceActivity extends AppCompatActivity {
+
+    private static final String TAG = "GEO-FENCING";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.geofence);
+    }
+
+    public void geofence(View view) {
+        Log.i(TAG, "Starting geo fencing");
+        TextView longitude = (TextView) findViewById(R.id.text_lng);
+        TextView latitude = (TextView) findViewById(R.id.text_lat);
+
+        TextView currentLocation = (TextView) findViewById(R.id.txt_info_current_location);
+        currentLocation.setText("Current set location - Lng: "+ longitude.getText() + ", Lat: "+ latitude.getText());
     }
 }
